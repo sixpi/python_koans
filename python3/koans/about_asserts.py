12e61b481bd1b0494b83f26cb76ceece68950fc0
Bing Xia <aix.bing@gmail.com>
2013-07-16 15:23:15 -0400

    Finish 3 lessons

--- python3/koans/about_asserts.py
+++ python3/koans/about_asserts.py
@@ -70,7 +70,7 @@ class AboutAsserts(Koan):
         #
         # See for yourself:
 
-        self.assertEqual(__, "naval".__class__) # It's str, not <type 'str'>
+        self.assertEqual(str, "naval".__class__) # It's str, not <type 'str'>
 
         # Need an illustration? More reading can be found here:
         #
